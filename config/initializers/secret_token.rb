f09227dd59cf34686eb2e69d2b63cf9bc1752e8a
matthew gamble <matt_gamble07@hotmail.com>
2011-11-09 09:24:12 -0600

    Updated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyworkspace::Application.config.secret_token = '207abc9c03cb9cfa0f94bb112ed932197f54badcb54e7c8670c0d8f96f16d64fefacc2f37b096853fe1dd2a4829e19cc6534cf25e169b216deaed2fd85aff326'
